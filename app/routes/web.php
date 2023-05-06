f3d204259e27b419c4a4f0251fd2f8794a904dfe
acacio90 <pedrorodrigues.2019@alunos.utfpr.edu.br>
2023-05-05 21:23:06 -0300

    :sparkles: feat: Adicionando rotas e retirando check de csrf

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\LoanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
+Route::get('/loans', [LoanController::class, 'index']);
+Route::get('/loans/{id}', [LoanController::class, 'show']);
+Route::post('/loans', [LoanController::class, 'store']);
+Route::put('/loans/{id}', [LoanController::class, 'update']);
+Route::delete('/loans/{id}', [LoanController::class, 'destroy']);
