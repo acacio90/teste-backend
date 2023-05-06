f4cd6106d1b9eaf37f061ba02065a8ff9cb2d3c5
acacio90 <pedrorodrigues.2019@alunos.utfpr.edu.br>
2023-05-05 21:19:52 -0300

    :sparkles: feat: Implementando controllers

---
+++ app/app/Http/Controllers/BookController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json($books);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json($book, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($request->all());
+
+        return $book;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return 204;
+    }
+}
---
+++ app/app/Http/Controllers/LoanController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Loan;
+use Illuminate\Http\Request;
+
+class LoanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $loans = Loan::all();
+        return response()->json($loans);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $loan = Loan::create($request->all());
+        return response()->json($loan, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $loan = Loan::find($id);
+        return response()->json($loan);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $loan = Loan::findOrFail($id);
+        $loan->update($request->all());
+
+        return $loan;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */     
+    public function destroy(string $id)
+    {
+        $loan = Loan::findOrFail($id);
+        $loan->delete();
+
+        return 204;
+    }
+}
